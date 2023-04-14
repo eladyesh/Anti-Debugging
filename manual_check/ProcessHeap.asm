d357a98143aaf97f892d43214c31bcc6d1bfb564
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-04-14 15:03:05 +0300

    Create ProcessHeap.asm

---
+++ manual_check/ProcessHeap.asm
@@ -0,0 +1,4 @@
+mov eax, large fs:30h   ; Move the value of the FS segment register at offset 30h (the PEB pointer) into the eax register
+mov eax, dword ptr [eax+18h] ; Move the value of the ProcessEnvironmentBlock (PEB) pointer (at offset 18h from the TEB pointer) into the eax register
+cmp dword ptr ds:[eax+10h], 0 ; Compare the value of the BeingDebugged flag (at offset 10h from the PEB pointer) with 0
+jne DebuggerDetected    ; Jump to the DebuggerDetected label if the BeingDebugged flag is not equal to 0 (i.e. a debugger is present)
