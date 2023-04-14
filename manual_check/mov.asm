55ff02e59281daba9d6358edadcdc6c009cc2271
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-04-14 14:54:06 +0300

    Update mov.asm

--- manual_check/mov.asm
+++ manual_check/mov.asm
@@ -1,4 +1,5 @@
-mov eax, dword ptr fs:[30h]
-mov ebx, byte ptr [eax+2]
-test ebx, ebx
-jz NoDebuggerDetected ; comment
+mov eax, dword ptr fs:[30h] ; Load the PEB (Process Environment Block) pointer into eax
+mov ebx, byte ptr [eax+2]   ; Load the value of the BeingDebugged flag from the PEB (Process Environment Block) at offset 2 from the TEB pointer into ebx
+test ebx, ebx               ; Test the value of the BeingDebugged flag (0 if no debugger is present, non-zero otherwise)
+jz NoDebuggerDetected       ; Jump to NoDebuggerDetected label if the flag is 0 (no debugger is present)
+
