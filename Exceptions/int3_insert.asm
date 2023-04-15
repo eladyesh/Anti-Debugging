0d912d8c778034dba7103dfb4f703248df42db3e
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-04-15 17:46:40 +0300

    Create int3_insert.asm

---
+++ Exceptions/int3_insert.asm
@@ -0,0 +1,11 @@
+; Push the address of "continue" label onto the stack
+push offset continue
+; Push the value of dword at the address pointed by fs:[0] onto the stack
+push dword fs:[0]
+; Move the current stack pointer to the location pointed by fs:[0]
+mov fs:[0], esp
+; Trigger a breakpoint interrupt (int 3)
+int 3
+
+; The code above checks if the program is being debugged
+; If it is, the breakpoint interrupt will cause the debugger to stop the program
